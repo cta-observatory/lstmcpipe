34c1d0c86645a53c48ebeab88e012a6e46d5c8ba
vuillaut <thomas.vuillaume@gmail.com>
2019-11-28 03:44:54 +0000

    copy config file in DL1 dir

--- lst_scripts/onsite_mc_merge_and_copy_dl1.py
+++ lst_scripts/onsite_mc_merge_and_copy_dl1.py
@@ -106,6 +106,11 @@ check_and_make_dir(final_DL1_dir)
 move_dir_content(running_DL1_dir, final_DL1_dir)
 print("DL1 files have been moved in {}".format(final_DL1_dir))
 
+# copy lstchain config file there too
+config_files = [os.path.join(running_DL1_dir, f) for f in os.listdir(running_DL1_dir) if f.endswith('.json')]
+for file in config_files:
+    shutil.copyfile(file,  os.path.join(final_DL1_dir, os.path.basename(file)))
+
 # 5. move running_dir as logs
 check_and_make_dir(logs_destination_dir)
 move_dir_content(input_dir, logs_destination_dir)
