c9a649e2f7a5885de6aa5b30dae9aaf80ff5b81b
vuillaut <vuillaume@lapp.in2p3.fr>
2020-06-12 09:06:17 +0100

    update benchs

--- lst_benchmarks/compare_dl1.py
+++ lst_benchmarks/compare_dl1.py
@@ -8,20 +8,27 @@ from lstchain.io.io import dl1_images_lstcam_key, dl1_params_lstcam_key, dl2_par
 from astropy.table import Table, join, hstack
 from datetime import date
 from matplotlib.backends.backend_pdf import PdfPages
+from matplotlib.colors import LogNorm
 
 pp = PdfPages(f'{date.today()}_dl1_compare_lstchain_hipecta.pdf')
 
 # filename_hipecta = '/fefs/aswg/workspace/thomas.vuillaume/mchdf5/run1/dl1_gamma_20deg_180deg_run1___cta-prod3-demo-2147m-LaPalma-baseline-mono_off0.4.h5'
 
 filename_hipecta = '/fefs/aswg/workspace/thomas.vuillaume/mchdf5/DL1/20190415/gamma/south_pointing/20200506_vRTA_v0.4.5_tailcuts_6_3_no_intercept/testing/dl1_reorganized_gamma_20deg_180deg_run1___cta-prod3-demo-2147m-LaPalma-baseline-mono_off0.4.h5'
-
-filename_lstchain = '/fefs/aswg/workspace/thomas.vuillaume/mchdf5/run1/lstchain/dl1_gamma_20deg_180deg_run1___cta-prod3-demo-2147m-LaPalma-baseline-mono_off0.4.simtel.h5'
+filename_hipecta = '/fefs/aswg/workspace/thomas.vuillaume/mchdf5/run1/dl1_recalculated_gamma_20deg_180deg_run1___cta-prod3-demo-2147m-LaPalma-baseline-mono_off0.4.h5'
+# filename_lstchain = '/fefs/aswg/workspace/thomas.vuillaume/mchdf5/run1/lstchain/dl1_gamma_20deg_180deg_run1___cta-prod3-demo-2147m-LaPalma-baseline-mono_off0.4.simtel.h5'
+filename_lstchain = '/fefs/aswg/workspace/thomas.vuillaume/mchdf5/DL1/20190415/gamma/south_pointing/20200529_vRTA_v0.5.1_v00/testing/dl1_reorganized_gamma_20deg_180deg_run1___cta-prod3-demo-2147m-LaPalma-baseline-mono_off0.4.h5'
 
 geom = CameraGeometry.from_name('LSTCam')
 geom2 = CameraGeometry.from_name('LSTCam-002')
 
-hp = Table.read(filename_hipecta, path=dl1_params_lstcam_key)
-hi = Table.read(filename_hipecta, path=dl1_images_lstcam_key)
+# hp = Table.read(filename_hipecta, path=dl1_params_lstcam_key)
+import pandas as pd
+df = pd.read_hdf(filename_hipecta, key=dl1_params_lstcam_key)
+hp = Table.from_pandas(df)
+
+hi = Table.read('/fefs/aswg/workspace/thomas.vuillaume/mchdf5/DL1/20190415/gamma/south_pointing/20200529_vRTA_v0.5.1_recalculated/dl1_recalculated_gamma_south_pointing_20200529_vRTA_v0.5.1_v00_DL1_t3st1ng_bckp.h5', path=dl1_images_lstcam_key)
+# hi = Table.read(filename_hipecta, path=dl1_images_lstcam_key)
 hi.rename_column('signal', 'image')
 hp['log_intensity'] = np.log10(hp['intensity'])
 
@@ -41,6 +48,7 @@ mega_table = join(lt, ht, keys=['event_id', 'tel_id'], uniq_col_name='{table_nam
 print(f"{len(mega_table)} events in common")
 
 filter = (mega_table['is_good_event'] == 1) & (mega_table['hipecta_n_islands'] == 1) # & (mega_table['lstchain_n_islands'] == 1) #
+# filter = np.ones(len(mega_table), dtype=bool)
 
 print(f"Only {np.count_nonzero(filter)} common events well reconstructed by hipecta will be included in the following plots")
 
@@ -59,13 +67,14 @@ for col_hipe in [c for c in selected_events.columns if 'hipecta_' in c]:
     both_finite = np.isfinite(selected_events[col_lst]) & np.isfinite(selected_events[col_hipe])
 
     if np.count_nonzero(both_finite) > 1:
-        axes[0].hist(selected_events[col_lst][both_finite], **opt, label='lstchain')
-        axes[0].hist(selected_events[col_hipe][both_finite], **opt, label='hipecta')
+        r = (selected_events[col_lst][both_finite].min(), selected_events[col_lst][both_finite].max())
+        axes[0].hist(selected_events[col_lst][both_finite], **opt, range=r, label='lstchain')
+        axes[0].hist(selected_events[col_hipe][both_finite], **opt, range=r, label='hipecta')
         axes[0].set_xlabel(feature_name, fontsize=16)
         axes[0].legend(fontsize=16)
         axes[0].grid()
 
-        axes[1].hist2d(selected_events[col_lst][both_finite], selected_events[col_hipe][both_finite], bins=100)
+        axes[1].hist2d(selected_events[col_lst][both_finite], selected_events[col_hipe][both_finite], bins=100, range=(r,r), norm=LogNorm())
         axes[1].set_xlabel('lstchain')
         axes[1].set_ylabel('hipecta')
         axes[1].set_title(feature_name)
--- lst_benchmarks/compare_dl2.py
+++ lst_benchmarks/compare_dl2.py
@@ -8,10 +8,12 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 from datetime import date
 import ctaplot
+from lstchain.io.io import read_simu_info_merged_hdf5
+import astropy.units as u
 
 savedir = 'plots'
 os.makedirs(savedir, exist_ok=True)
-pp = PdfPages(f'compare_lstchain_hipecta_dl2_{date.today()}.pdf')
+pp = PdfPages(f'{date.today()}_dl2_compare_lstchain_hipecta.pdf')
 
 
 lstchain_d = {}
@@ -20,10 +22,18 @@ lstchain_d['dir'] = '../20200229_v0.4.4/lstchain'
 hipecta_d['dir'] = '.'
 
 # La Palma
-lstchain_d['dir'] = '/fefs/aswg/data/mc/DL2/20190415/{particle}/south_pointing/20200316_v0.4.5__EG1/'
-# hipecta_d['dir'] = '/fefs/aswg/workspace/thomas.vuillaume/mchdf5/DL2/20190415/{particle}/south_pointing/20200407_vRTA_no_intercept/'
-hipecta_d['dir'] = '/fefs/aswg/workspace/thomas.vuillaume/mchdf5/DL2/20190415/gamma/south_pointing/20200411_vRTA_v0.4.5.post117+git62d122d_test_dl1_dl2/'
 
+lstchain_dir_id = '20200527_v0.5.1_global'
+hipecta_dir_id = '20200529_vRTA_v0.5.1_recalculated'
+
+# lstchain_d['dir'] = '/fefs/aswg/data/mc/DL2/20190415/{particle}/south_pointing/20200316_v0.4.5__EG1/'
+lstchain_d['dir'] = os.path.join('/fefs/aswg/data/mc/DL2/20190415/{particle}/south_pointing/', lstchain_dir_id)
+# hipecta_d['dir'] = '/fefs/aswg/workspace/thomas.vuillaume/mchdf5/DL2/20190415/{particle}/south_pointing/20200407_vRTA_no_intercept/'
+# hipecta_d['dir'] = '/fefs/aswg/workspace/thomas.vuillaume/mchdf5/DL2/20190415/{particle}/south_pointing/20200410_vRTA_v0.4.5.post117+git62d122d_tailcuts_6_3_no_intercept/'
+hipecta_d['dir'] = os.path.join('/fefs/aswg/workspace/thomas.vuillaume/mchdf5/DL2/20190415/{particle}/south_pointing/', hipecta_dir_id)
+
+
+filename_simu = '/fefs/aswg/workspace/thomas.vuillaume/mchdf5/DL2/20190415/gamma/south_pointing/20200529_vRTA_v0.5.1_v00/dl2_gamma_south_pointing_20200529_vRTA_v0.5.1_v00_DL1_testing.h5'
 
 particles = ['gamma', 'proton', 'electron']
 
@@ -32,13 +42,16 @@ for dic in [lstchain_d, hipecta_d]:
     for particle in particles:
         particle_dir = dic['dir'].format(particle=particle)
         filename = [os.path.join(particle_dir, f) for f in os.listdir(particle_dir) if 'testing' in f][0]
-        dic[particle] = pd.read_hdf(filename, key=dl2_params_lstcam_key)
+        dic[particle] = {}
+        dic[particle]['filename'] = filename
+        dic[particle]['simu_info'] = read_simu_info_merged_hdf5(filename_simu)
+        dic[particle]['events'] = pd.read_hdf(filename, key=dl2_params_lstcam_key)
         
         # some rescaling 
-        if dic is hipecta_d:
-            dic[particle]['reco_energy'] *= 1e3
-            dic[particle]['log_mc_energy'] += 3
-            dic[particle]['log_reco_energy'] += 3
+#         if dic is hipecta_d:
+#             dic[particle]['events']['reco_energy'] *= 1e3
+#             dic[particle]['events']['log_mc_energy'] += 3
+#             dic[particle]['events']['log_reco_energy'] += 3
 
         
 # # laptop where all files are in the same dir
@@ -61,15 +74,60 @@ for dic in [lstchain_d, hipecta_d]:
 # pp.savefig()
 
 
+common_filters = ['gammaness > 0.1', 'intensity > 200', 'tel_id==1', 'r<1']
+print(common_filters)
 
 for p in particles:
-    filter_good_events = hipecta_d[p]['is_good_event'] == 1
-    hipe = hipecta_d[p][filter_good_events]
-    lst = lstchain_d[p]
+    filter_good_events = hipecta_d[p]['events']['is_good_event'] == 1
+    hipe = hipecta_d[p]['events'][filter_good_events]
+    lst = lstchain_d[p]['events']
+    
+    print(f"{len(hipecta_d[p]['events'])} events in hipecta file and {len(hipe)} good events")
+    
+    for f in common_filters:
+        hipe = hipe.query(f)
+        lst = lst.query(f)
+        
+    print(f"{len(hipe)} events in hipecta after common filters")
+    print(f"{len(lst)} events in lstchain after common filters")
 
     if p == 'gamma':
-        hipe_sel = hipe[(hipe.gammaness > 0.7) & (hipe.leakage < 0.2) & (hipe.intensity > 200)]
-        lst_sel = lst[(lst.gammaness > 0.7) & (lst.leakage < 0.2) & (lst.intensity > 200)]
+        hipe_sel = hipe[(hipe.intensity > 200)
+                        & (hipe.gammaness > 0.6)
+#                         & (hipe.leakage < 0.2) 
+#                         & (hipe.n_islands==1)
+                        #&
+#                         (hipe.wl > 0.2) &
+#                         (hipe.wl < 0.98) &
+#                         (hipe.skewness > 0) &
+#                         (hipe.r < 0.6)
+                       ]
+        lst_sel = lst[(lst.intensity > 200) 
+                      & (lst.gammaness > 0.7) 
+#                       & (lst.leakage < 0.2) 
+#                       & (lst.n_islands==1)
+#                       & (lst.wl > 0.2)
+#                       & (lst.wl < 0.98)
+#                       & (lst.r < 0.6) 
+#                       & (lst.skewness > 0)
+                     ]
+        
+        print(f"{len(hipe_sel)} events in hipecta after specific filters")
+        print(f"{len(lst_sel)} events in lstchain after specific filters")
+        
+        text = f"{len(hipecta_d[p]['events'])} events in hipecta file and {len(hipe)} good events\n"
+        text += f"common filters:\n{common_filters}\n"
+        text += f"{len(hipe)} events in hipecta after common filters\n"
+        text += f"{len(lst)} events in lstchain after common filters\n"
+        text += f"{len(hipe_sel)} events in hipecta after specific filters\n"
+        text += f"{len(lst_sel)} events in lstchain after specific filters\n"
+        
+        firstPage = plt.figure(figsize=(11.69,8.27))
+        firstPage.clf()
+        firstPage.text(0.2,0.5, text, transform=firstPage.transFigure, size=11, ha="center")
+        pp.savefig()
+        plt.close()
+        
         plt.figure(figsize=(15,10))
         ctaplot.plot_energy_resolution(hipe_sel.mc_energy, hipe_sel.reco_energy, label='hipecta')
         ctaplot.plot_energy_resolution(lst_sel.mc_energy, lst_sel.reco_energy, label='lstchain')
@@ -101,6 +159,55 @@ for p in particles:
         pp.savefig()
 
         plt.close('all')
+        
+    
+    plt.figure(figsize=(15,10))
+    simu_info = hipecta_d[p]['simu_info']
+    ctaplot.plot_effective_area_per_energy_power_law(simu_info.energy_range_min.to(u.TeV).value,
+                                                     simu_info.energy_range_max.to(u.TeV).value,
+                                                     simu_info.shower_reuse * simu_info.num_showers,
+                                                     simu_info.spectral_index,
+                                                     hipe.query('tel_id==1').reco_energy,
+                                                     np.pi*(simu_info.max_scatter_range.to(u.m)**2-simu_info.min_scatter_range.to(u.m)**2).value,
+                                                     linestyle='--',
+                                                     label = 'hipecta'
+                                                    )
+    ctaplot.plot_effective_area_per_energy_power_law(simu_info.energy_range_min.to(u.TeV).value,
+                                                     simu_info.energy_range_max.to(u.TeV).value,
+                                                     simu_info.shower_reuse * simu_info.num_showers,
+                                                     simu_info.spectral_index,
+                                                     hipe_sel.query('tel_id==1').reco_energy,
+                                                     np.pi*(simu_info.max_scatter_range.to(u.m)**2-simu_info.min_scatter_range.to(u.m)**2).value,
+                                                     label = 'hipecta specific selection'
+                                                    )
+    
+    simu_info = lstchain_d[p]['simu_info']
+    ctaplot.plot_effective_area_per_energy_power_law(simu_info.energy_range_min.to(u.TeV).value,
+                                                     simu_info.energy_range_max.to(u.TeV).value,
+                                                     simu_info.shower_reuse * simu_info.num_showers,
+                                                     simu_info.spectral_index,
+                                                     lst.query('tel_id==1').reco_energy,
+                                                     np.pi*(simu_info.max_scatter_range.to(u.m)**2-simu_info.min_scatter_range.to(u.m)**2).value,
+                                                     label = 'lstchain',
+                                                     linestyle='--'
+                                                    )
+    ctaplot.plot_effective_area_per_energy_power_law(simu_info.energy_range_min.to(u.TeV).value,
+                                                     simu_info.energy_range_max.to(u.TeV).value,
+                                                     simu_info.shower_reuse * simu_info.num_showers,
+                                                     simu_info.spectral_index,
+                                                     lst_sel.query('tel_id==1').reco_energy,
+                                                     np.pi*(simu_info.max_scatter_range.to(u.m)**2-simu_info.min_scatter_range.to(u.m)**2).value,
+                                                     label = 'lstchain specific selection'
+                                                    )
+    
+    print(simu_info.spectral_index)
+    
+    ctaplot.plot_effective_area_cta_performance('north', color='black')
+    plt.legend(fontsize=18)
+    plt.grid()
+    plt.tight_layout()
+    pp.savefig()   
+    plt.close()
 
     fig, axes = plt.subplots(1, 2, figsize=(15,6))
     fig.suptitle(p)
@@ -149,8 +256,15 @@ for p in particles:
     for c in set(lst.columns).intersection(set(hipe.columns)):
 
         fig = plt.figure(figsize=(15,10))
-        lst[c].hist(bins=100, histtype='step', label='lstchain', log=True, linewidth=3)
-        hipe[c].hist(bins=100, histtype='step', label='hipecta', log=True, linewidth=3)
+        opt = dict(bins=100, histtype='step', log=True, linewidth=3)
+        lst[c].hist(label='lstchain', **opt)
+        hipe[c].hist(label='hipecta', **opt)
+
+        mc_c = c.replace('reco_', 'mc_')
+        if  mc_c in set(lst.columns) and mc_c != c:
+            lst[mc_c].hist(**opt, label='MC', color='black', linestyle='--')
+
+                
         plt.legend(fontsize=15)
         plt.title(f'{p} {c}')
         pp.savefig()
