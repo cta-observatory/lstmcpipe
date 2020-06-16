f0bb94067f02b669d4c46181e09cda9b9a975426
Enrique <garcia@lapp.in2p3.fr>
2020-06-16 21:05:50 +0200

    add various test to the reorganizer test

--- tests/test_reorganize_hipecta_dl1.py
+++ tests/test_reorganize_hipecta_dl1.py
@@ -3,9 +3,10 @@
 import os
 import tables
 import argparse
+import numpy as np
 from distutils.util import strtobool
 from astropy.table import Table
-from hiperta_r0_to_dl1lstchain import main as reorganize_dl1_hiperta
+from lst_scripts.hiperta_r0_to_dl1lstchain import main as reorganize_dl1_hiperta
 
 parser = argparse.ArgumentParser(description="Test the hipecta to lstchain dl1 file converted")
 
@@ -13,6 +14,7 @@ parser.add_argument('--infile', '-i',
                     type=str,
                     dest='infile',
                     help='Output of `hiperta_r1_dl1 file to test',
+                    default='/Users/garciaenrique/CTA/data/LST_mono/dl1_gamma_20190415_20_0_run100_Tel_1_1_Tel_1_0.h5'
                     )
 
 parser.add_argument('--outdir', '-o',
@@ -44,7 +46,8 @@ def test_reorganize_dl1hiperta_to_dl1lstchain():
     """
     # We know in advance the output names
     # gamma.h5 --> dl1_gamma.h5 (after hiperta_r1_dl1) --> dl1_reorganized_gamma.h5 (after reorganizer script).
-    assert args.infile.find('dl1_') == 0
+    print(args.infile)
+    #assert args.infile.find('dl1_') == 0
     base_filename = args.infile[4:]
     output_reorganized_filename = os.path.join(args.outdir, "dl1_reorganized_" + os.path.basename(base_filename))
     if not os.path.isfile(output_reorganized_filename):
@@ -65,13 +68,25 @@ def test_reorganize_dl1hiperta_to_dl1lstchain():
     except:
         tel_ids = [i[0] + 1 for i in enumerate(dl1)]
 
+    mc_events = Table(hf.root.simulation.mc_event.read())
     params_hipecta = [Table(tel.parameters.read()) for tel in dl1]
-    tels_reorg = Table(dl1_reorg.event.telescope.parameters.LST_LSTCam.read())
+    images_hipecta = [Table(tel.calib_pic.read()) for tel in dl1]
+    reorganized_table = Table(dl1_reorg.event.telescope.parameters.LST_LSTCam.read())
 
     # Check all the events from the hipecta file have been copied correctly
     for i, tel in enumerate(tel_ids):
-        assert len(params_hipecta[i]) == len(tels_reorg[tels_reorg['tel_id'] == tel]['tel_id'])
-
-    # check log intensity
-    # check wl is w / l
-    # check images
+        assert len(params_hipecta[i]) == \
+            len(reorganized_table[reorganized_table['tel_id'] == tel]['tel_id'])
+        # check log intensity
+        assert np.log10(params_hipecta[i]['intensity']).all() == \
+            reorganized_table[reorganized_table['tel_id'] == tel]['log_intensity'].all()
+        # check wl is w / l
+        assert (params_hipecta[i]['width'] / params_hipecta[i]['length']).all() == \
+            reorganized_table[reorganized_table['tel_id'] == tel]['wl'].all()
+        # check images were well stacked
+        assert images_hipecta[i]['event_id'].all() == \
+            reorganized_table[reorganized_table['tel_id'] == tel]['event_id'].all()
+        # check the join of the `mc_events` tables
+        for col in mc_events.itercols():
+            assert set(reorganized_table[reorganized_table['tel_id'] == tel][mc_events.colnames][col]).issubset(
+                mc_events[col])
