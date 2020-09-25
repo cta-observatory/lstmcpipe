d19f329b5f9529be0d2a36c0a48a280c16c00964
Enrique Garcia <40355845+garciagenrique@users.noreply.github.com>
2020-09-25 15:09:40 +0200

    Upgrade lstc060 (#49)
    * add lstchain config file v060
    
    * move files

--- lst_scripts/deprecated/master.py
+++ lst_scripts/deprecated/master.py
@@ -8,7 +8,7 @@ TODO:
 - user still need to wait for each step to execute and check output to go to the next one
 """
 
-
+# Kike: Personally (imho) I would prefer to make a .yml config file and read everything from it
 
 ### global variables - do not change unless you know what you are doing
 # BASE_DIR = '/fefs/aswg/'
