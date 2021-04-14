7694adc0f9886932bceb2de869f907a56f60fc59
Enrique <garcia@lapp.in2p3.fr>
2021-04-14 15:57:25 +0200

    rename dirs, and modify license, readme and requirements with correct path and names

--- lst_scripts/tests/test_install.py
+++
@@ -1,7 +0,0 @@
-import lst_scripts
-import re
-
-def test_version():
-    with open("lst_scripts/__init__.py") as f:
-        __version__ = re.search('^__version__ = "(.*)"$', f.read()).group(1)
-    assert lst_scripts.__version__ == __version__
