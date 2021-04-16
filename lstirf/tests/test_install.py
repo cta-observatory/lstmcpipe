98a9eca1bd9c18c3793e6f743f30a5d8cf6f6279
Enrique <garcia@lapp.in2p3.fr>
2021-04-16 17:50:06 +0200

    rename module and lib

--- lstirf/tests/test_install.py
+++
@@ -1,8 +0,0 @@
-import re
-import lst_scripts
-
-
-def test_version():
-    with open("lstirf/__init__.py") as f:
-        __version__ = re.search('^__version__ = "(.*)"$', f.read()).group(1)
-    assert lst_scripts.__version__ == __version__
