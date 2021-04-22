718cb386b2b6a6c905af437241c7dd248ca190f3
vuillaut <thomas.vuillaume@gmail.com>
2021-04-23 00:26:53 +0200

    fix test install

--- lstmcpipe/tests/test_install.py
+++ lstmcpipe/tests/test_install.py
@@ -3,6 +3,6 @@ import lstmcpipe
 
 
 def test_version():
-    with open("lstirf/__init__.py") as f:
+    with open("lstmcpipe/__init__.py") as f:
         __version__ = re.search('^__version__ = "(.*)"$', f.read()).group(1)
     assert lstmcpipe.__version__ == __version__
