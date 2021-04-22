0766dcd2f8a28efb12dec8a7abb330be7da07eb4
Enrique <garcia@lapp.in2p3.fr>
2021-04-22 17:06:53 +0200

    correct relative imports

--- lstmcpipe/onsite_mc_r0_to_dl1.py
+++ lstmcpipe/onsite_mc_r0_to_dl1.py
@@ -15,7 +15,7 @@ import random
 import argparse
 import calendar
 import lstchain
-from .io.data_management import (
+from lstmcpipe.io.data_management import (
     check_data_path,
     get_input_filelist,
     check_and_make_dir,
