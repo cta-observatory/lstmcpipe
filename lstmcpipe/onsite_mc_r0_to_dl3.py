68c4b57673577fc3e3e9818e2a20e914f9f76221
Enrique <garcia@lapp.in2p3.fr>
2021-04-28 18:32:00 +0200

    add absolute path when passing the conf so that batch commands can be batched from anywhere

--- lstmcpipe/onsite_mc_r0_to_dl3.py
+++ lstmcpipe/onsite_mc_r0_to_dl3.py
@@ -16,6 +16,7 @@
 
 import sys
 import argparse
+from os.path import abspath
 from distutils.util import strtobool
 from lstmcpipe.workflow_management import (
     batch_r0_to_dl1,
@@ -106,7 +107,7 @@ if __name__ == '__main__':
 
             log_batch_r0_dl1, debug_r0dl1, jobs_all_r0_dl1 = batch_r0_to_dl1(
                 dl0_data_dir,
-                args.config_file_lst,
+                abspath(args.config_file_lst),
                 prod_id,
                 all_particles,
                 source_env=source_env,
@@ -117,10 +118,10 @@ if __name__ == '__main__':
 
             log_batch_r0_dl1, debug_r0dl1, jobs_all_r0_dl1 = batch_r0_to_dl1_rta(
                 dl0_data_dir,
-                args.config_file_rta,
+                abspath(args.config_file_rta),
                 prod_id,
                 all_particles,
-                args.config_file_lst,
+                abspath(args.config_file_lst),
                 gamma_offsets=gamma_offs
             )
 
@@ -166,7 +167,7 @@ if __name__ == '__main__':
 
         log_batch_train_pipe, job_from_train_pipe, model_dir, debug_train = batch_train_pipe(
             log_batch_merge_and_copy,
-            args.config_file_lst,
+            abspath(args.config_file_lst),
             jobs_to_train,
             source_env=source_env
         )
@@ -185,7 +186,7 @@ if __name__ == '__main__':
         log_batch_dl1_to_dl2, jobs_from_dl1_dl2, debug_dl1dl2 = batch_dl1_to_dl2(
             dl1_data_dir,
             model_dir,
-            args.config_file_lst,
+            abspath(args.config_file_lst),
             job_from_train_pipe,       # Single jobid from train
             jobs_all_dl1_finished,     # jobids from merge
             log_batch_merge_and_copy,  # final dl1 names
@@ -208,7 +209,7 @@ if __name__ == '__main__':
             dl2_data_dir,
             all_particles,
             gamma_offs,
-            args.config_file_lst,
+            abspath(args.config_file_lst),
             jobs_from_dl1_dl2,
             log_from_dl1_dl2=log_batch_dl1_to_dl2,
             source_env=source_env,
