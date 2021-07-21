425cecb491201ef7de1662571ef8c1472e283d28
Lukas Nickel <lukas.nickel@tu-dortmund.de>
2021-07-21 14:13:45 +0200

    Basic test case
    - Ensures I dont mess up the code when refactoring the config parsing

--- lstmcpipe/config/tests/test_config.py
+++ lstmcpipe/config/tests/test_config.py
@@ -1,10 +1,18 @@
-from lstmcpipe.config import export_env
+from lstmcpipe.config import export_env, parse_config_and_handle_global_vars
 import tempfile
 import os
-
+import yaml
 
 def test_export_env():
     tmp_dir = tempfile.mkdtemp()
     export_env(tmp_dir)
     if not os.path.exists(os.path.join(tmp_dir, 'requirements.txt')):
         assert os.path.exists(os.path.join(tmp_dir, 'conda_env.yml'))
+
+
+def test_parse_config_and_handle_global_vars():
+    config_path = "lstmcpipe/config_MC_prod.yml"
+    config = parse_config_and_handle_global_vars(config_path)
+    with open("lstmcpipe/config/tests/expected.yaml") as e:
+        expected = yaml.safe_load(e)
+    assert config == expected
