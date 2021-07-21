31dac03c8d3cdc85c94aee2adc541194c09e17d1
Lukas Nickel <lukas.nickel@tu-dortmund.de>
2021-07-21 14:46:11 +0200

    Small cleanup
    - Getting rid of some duplicated code
    - Only change in behaviour: If prod_id is meant to be the default v_00,
      omit the key from the config file instead of explicitly setting it to
      None

--- lstmcpipe/config/pipeline_config.py
+++ lstmcpipe/config/pipeline_config.py
@@ -3,6 +3,8 @@ from pathlib import Path
 import yaml
 import calendar
 import lstchain
+import logging
+
 
 def load_yml_config(yml_file):
     """
@@ -54,13 +56,13 @@ def parse_config_and_handle_global_vars(yml_file):
 
     # Load configuration
     workflow_kind = loaded_config['workflow_kind']
-    custom_prod_id = loaded_config['prod_id']
+    custom_prod_id = loaded_config.get('prod_id', 'v00')
     stages_to_be_run = loaded_config['stages_to_be_run']
     merging_options = loaded_config['merging_options']['no_image']
 
     base_path_dl0 = loaded_config['base_path_dl0']
     prod_type = loaded_config['prod_type']
-    obs_date = str(loaded_config['obs_date'])
+    obs_date = loaded_config['obs_date']
     pointing = loaded_config['pointing']
     zenith = loaded_config['zenith']
     particles = loaded_config['particles']
@@ -68,15 +70,15 @@ def parse_config_and_handle_global_vars(yml_file):
 
     # Check allowed cases
     if workflow_kind not in allowed_workflows or prod_type not in allowed_prods:
-        print(f'\nPlease select an \n\t - allowed `workflow_kind`: {allowed_workflows} or an \n\t - allowed production '
-              f'type: {allowed_prods} in the config YAML file; {yml_file}.')
-        sys.exit(-1)
+        raise ValueError(f'Please select an allowed `workflow_kind`: {allowed_workflows} or an - allowed '
+                         f'production type: {allowed_prods} in the config YAML file; {yml_file}.'
+                         )
 
     # and incompatible possibilities
     if (prod_type == 'prod3' and obs_date != '20190415') or (prod_type == 'prod5' and obs_date == '20190415'):
-        print(f'\nThis prod_type and obs_date combination is not possible.\n'
-              f'Please change it in the config YAML file; {yml_file}.')
-        sys.exit(-1)
+        raise ValueError('This prod_type and obs_date combination is not possible.'
+                         f'Please change it in the config YAML file; {yml_file}.'
+                         )
 
     # Prod_id syntax
     today = calendar.datetime.date.today()
@@ -89,17 +91,14 @@ def parse_config_and_handle_global_vars(yml_file):
         # TODO parse version from hiPeRTA module
         base_prod_id = f'{today.year:04d}{today.month:02d}{today.day:02d}_vRTA300_v{lstchain.__version__}'
     else:
-        print(f'\n\tPlease select an allowed `workflow_kind`: {allowed_workflows} in the config YAML file; {yml_file}.')
-        sys.exit(-1)
+        raise ValueError('Please select an allowed `workflow_kind`: {allowed_workflows} in the config YAML file; {yml_file}.')
 
     # Create the final config structure to be passed to the pipeline
     # 1 - Prod_id
     if 'trans_80' in obs_date:
-        suffix_id = '_{}_trans_80_v00'.format(prod_type) if custom_prod_id is None else '_{}_trans_80_{}'.format(
-            prod_type, custom_prod_id)
+        suffix_id = '_{}_trans_80_{}'.format(prod_type, custom_prod_id)
     else:
-        suffix_id = '_{}_v00'.format(prod_type) if custom_prod_id is None else '_{}_{}'.format(prod_type,
-                                                                                               custom_prod_id)
+        suffix_id = '_{}_{}'.format(prod_type, custom_prod_id)
     config['prod_id'] = base_prod_id + suffix_id
 
     # 2 - Parse source environment correctly
@@ -115,8 +114,9 @@ def parse_config_and_handle_global_vars(yml_file):
     elif obs_date == '20190415' or prod_type == 'prod3':
         config['gamma_offs'] = None
     else:
-        print(f'\n\tPlease select an \n\tallowed `obs_date`: {allowed_obs_date} in the config YAML file; {yml_file}.')
-        sys.exit(-1)
+        raise ValueError(
+            f'Please select an allowed `obs_date`: {allowed_obs_date} in the config YAML file; {yml_file}.'
+        )
 
     # 4 - Stages to be run
     config['stages_to_run'] = stages_to_be_run
@@ -128,83 +128,58 @@ def parse_config_and_handle_global_vars(yml_file):
 
     # 6 - Global paths
     if prod_type == 'prod3':
-        if workflow_kind == 'lstchain' or workflow_kind =='ctapipe':
-            config['DL0_data_dir'] = os.path.join(base_path_dl0, 'DL0', obs_date, '{}', pointing)
-        else:  # RTA
-            config['DL0_data_dir'] = os.path.join(base_path_dl0, 'R0', obs_date, '{}', pointing)
-
-        config['running_analysis_dir'] = os.path.join(
-            base_path_dl0, 'running_analysis', obs_date, '{}', pointing, config['prod_id']
-        )
-        config['analysis_log_dir'] = os.path.join(
-            base_path_dl0, 'analysis_logs', obs_date, '{}', pointing, config['prod_id']
-        )
-        config['DL1_data_dir'] = os.path.join(
-            base_path_dl0, 'DL1', obs_date, '{}', pointing, config['prod_id']
-        )
-        config['DL2_data_dir'] = os.path.join(
-            base_path_dl0, 'DL2', obs_date, '{}', pointing, config['prod_id']
-        )
-        config['IRFs_dir'] = os.path.join(
-            base_path_dl0, 'IRF', obs_date, pointing, config['prod_id']
-        )
-
-        if base_path_dl0 == '/fefs/aswg/data/mc':  # lstanalyzer user
-            config['model_dir'] = os.path.join('/fefs/aswg/data/', 'models', obs_date, pointing, config['prod_id'])
-        else:
-            # user case, model dir in same dir as DL0, DL1, DL2, running...
-            config['model_dir'] = os.path.join(base_path_dl0, 'models', obs_date, pointing, config['prod_id'])
+        p = pointing
 
     else:  # Prod5
         # TODO correct the path for all the prod5_* at the LP_cluster
         #  as they were run as $ZENITH/$POINTING instead of $POINTING/$ZENITH/
-
+        p = os.path.join(zenith, pointing)
         config['gammas_offsets'] = offset_gammas
 
-        if workflow_kind == 'lstchain' or workflow_kind =='ctapipe':
-            config['DL0_data_dir'] = os.path.join(base_path_dl0, 'DL0', obs_date, '{}', zenith, pointing)
-        else:  # RTA
-            config['DL0_data_dir'] = os.path.join(base_path_dl0, 'R0', obs_date, '{}', zenith, pointing)
+    if workflow_kind == 'lstchain' or workflow_kind =='ctapipe':
+        config['DL0_data_dir'] = os.path.join(base_path_dl0, 'DL0', obs_date, '{}', p)
+    else:  # RTA
+        config['DL0_data_dir'] = os.path.join(base_path_dl0, 'R0', obs_date, '{}', p)
 
-        config['running_analysis_dir'] = os.path.join(
-            base_path_dl0, 'running_analysis', obs_date, '{}', zenith, pointing, config['prod_id']
-        )
-        config['analysis_log_dir'] = os.path.join(
-            base_path_dl0, 'analysis_logs', obs_date, '{}', zenith, pointing, config['prod_id']
-        )
-        config['DL1_data_dir'] = os.path.join(
-            base_path_dl0, 'DL1', obs_date, '{}', zenith, pointing, config['prod_id']
-        )
-        config['DL2_data_dir'] = os.path.join(
-            base_path_dl0, 'DL2', obs_date, '{}', zenith, pointing, config['prod_id']
-        )
-        config['IRFs_dir'] = os.path.join(
-            base_path_dl0, 'IRF', obs_date, zenith, pointing, config['prod_id']
-        )
+    config['running_analysis_dir'] = os.path.join(
+        base_path_dl0, 'running_analysis', obs_date, '{}', p, config['prod_id']
+    )
+    config['analysis_log_dir'] = os.path.join(
+        base_path_dl0, 'analysis_logs', obs_date, '{}', p, config['prod_id']
+    )
+    config['DL1_data_dir'] = os.path.join(
+        base_path_dl0, 'DL1', obs_date, '{}', p, config['prod_id']
+    )
+    config['DL2_data_dir'] = os.path.join(
+        base_path_dl0, 'DL2', obs_date, '{}', p, config['prod_id']
+    )
+    config['IRFs_dir'] = os.path.join(
+        base_path_dl0, 'IRF', obs_date, p, config['prod_id']
+    )
 
-        if base_path_dl0 == '/fefs/aswg/data/mc':  # lstanalyzer user
-            config['model_dir'] = os.path.join(
-                '/fefs/aswg/data/', 'models', obs_date, zenith, pointing, config['prod_id']
-            )
-        else:
-            # user case, model dir in same dir as DL0, DL1, DL2, running...
-            config['model_dir'] = os.path.join(base_path_dl0, 'models', obs_date, zenith, pointing, config['prod_id'])
+    if base_path_dl0 == '/fefs/aswg/data/mc':  # lstanalyzer user
+        config['model_dir'] = os.path.join(
+            '/fefs/aswg/data/', 'models', obs_date, p, config['prod_id']
+        )
+    else:
+        # user case, model dir in same dir as DL0, DL1, DL2, running...
+        config['model_dir'] = os.path.join(base_path_dl0, 'models', obs_date, p, config['prod_id'])
 
     # print the PATH and prod_id confirmation
 
-    print(f'\n\n\t ************ - {workflow_kind} {prod_type} - WORKFLOW KIND - ************ \n\n'
-          f'\nSimtel DL0 files are going to be searched at  \n\n   '
-          f'\t{config["DL0_data_dir"].format(str("""{""") + ",".join(config["all_particles"]) + str("""}"""))}\n\n'
-          f'The following directories and all the information within them will be either created or overwritten:\n'
-          f'[subdirectories with a same PROD_ID and analysed the same day]\n\n'
-          f'\t{config["running_analysis_dir"].format(str("""{""") + ",".join(config["all_particles"]) + str("""}"""))}\n'
-          f'\t{config["DL1_data_dir"].format(str("""{""") + ",".join(config["all_particles"]) + str("""}"""))}\n'
-          f'\t{config["DL2_data_dir"].format(str("""{""") + ",".join(config["all_particles"]) + str("""}"""))}\n'
-          f'\t{config["analysis_log_dir"].format(str("""{""") + ",".join(config["all_particles"]) + str("""}"""))}\n'
-          f'\t{config["IRFs_dir"]}\n'
-          f'\t{config["model_dir"]}\n'
-          f'\n\tPROD_ID to be used: {config["prod_id"]}\n'
-          )
+#    print(f'\n\n\t ************ - {workflow_kind} {prod_type} - WORKFLOW KIND - ************ \n\n'
+#          f'\nSimtel DL0 files are going to be searched at  \n\n   '
+#          f'\t{config["DL0_data_dir"].format(str("""{""") + ",".join(config["all_particles"]) + str("""}"""))}\n\n'
+#          f'The following directories and all the information within them will be either created or overwritten:\n'
+#          f'[subdirectories with a same PROD_ID and analysed the same day]\n\n'
+#          f'\t{config["running_analysis_dir"].format(str("""{""") + ",".join(config["all_particles"]) + str("""}"""))}\n'
+#          f'\t{config["DL1_data_dir"].format(str("""{""") + ",".join(config["all_particles"]) + str("""}"""))}\n'
+#          f'\t{config["DL2_data_dir"].format(str("""{""") + ",".join(config["all_particles"]) + str("""}"""))}\n'
+#          f'\t{config["analysis_log_dir"].format(str("""{""") + ",".join(config["all_particles"]) + str("""}"""))}\n'
+#          f'\t{config["IRFs_dir"]}\n'
+#          f'\t{config["model_dir"]}\n'
+#          f'\n\tPROD_ID to be used: {config["prod_id"]}\n'
+#          )
 
     print("Stages to be run:")
     for stage in config['stages_to_run']:
