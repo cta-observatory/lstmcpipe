cec419f4aa75c58b6417fba4e374967effa6e092
Enrique Garcia <garcia@lapp.in2p3.fr>
2021-07-22 14:43:29 +0200

    move scripts to submodule, renaming onsite_mc_r0_dl3 and create entry points

--- setup.py
+++ setup.py
@@ -28,12 +28,13 @@ scripts_list = find_scripts('lstmcpipe', 'onsite_')
 
 entry_points = {
     'console_scripts': [
+        'lstmcpipe = lstmcpipe.lstmcpipe_start:main',
         'lstmcpipe_plot_models_importance = lstmcpipe.plots.plot_models_importance:main',
         'lstmcpipe_plot_irfs = lstmcpipe.plots.plot_irfs:main',
         'lstmcpipe_dl2_to_sensitivity = lstmcpipe.scripts.script_dl2_to_sensitivity:main',
         'lstmcpipe_hiperta_r0_to_dl1lstchain = lstmcpipe.hiperta.hiperta_r0_to_dl1lstchain:main',
-        'lstmcpipe_utils_move_dir = lstmcpipe.stages.script_merge_utils_move_dir:main',
-        'lstmcpipe_utils_cp_config = lstmcpipe.stages.script_merge_utils_copy_config:main'
+        'lstmcpipe_utils_move_dir = lstmcpipe.scripts.script_merge_utils_move_dir:main',
+        'lstmcpipe_utils_cp_config = lstmcpipe.scripts.script_merge_utils_copy_config:main'
     ]
 }
 
