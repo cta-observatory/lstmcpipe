e892191ac0291d1c9f974efda00b0619cec280f1
Lukas Nickel <lukas.nickel@tu-dortmund.de>
2021-08-03 12:18:35 +0200

    create script batch files

---
+++ lstmcpipe/scripts/script_batch_filelist_rta.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import argparse
+import subprocess
+from distutils.util import strtobool
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Batches the r0_to_dl1 hiperta stage for all the files "
+                                                 "within a text file.")
+    parser.add_argument('--file_list', '-f', type=str,
+                        dest='file_list',
+                        help='Path to file containing list of R0 lstchain file to be processed.',
+                        required=True)
+    parser.add_argument('--output_dir', '-o', type=str,
+                        dest='output_dir',
+                        help='hiperta_r0_to_dl1 output directory argument.',
+                        required=True)
+    parser.add_argument('--config_file', '-c', type=str,
+                        dest='config_file',
+                        help='hiperta_r0_to_dl1 configuration file argument.',
+                        required=True)
+    parser.add_argument('--keep_file', '-k',
+                        type=lambda x: bool(strtobool(x)),
+                        dest='keep_file',
+                        help='Keep output of hiperta. Set by default to False',
+                        default=False
+                        )
+    parser.add_argument('--debug_mode', '-d',
+                        type=lambda x: bool(strtobool(x)),
+                        dest='debug_mode',
+                        help='Activate debug mode (add cleaned mask in the output hdf5). Set by default to False',
+                        default=False
+                        )
+    args = parser.parse_args()
+
+    with open(args.file_list, 'r') as filelist:
+        for file in filelist:
+
+            file = file.strip('\n')
+
+            cc = '--config {}'.format(args.config_file) if args.config_file is not None else ' '
+
+            cmd = [f'lstmcpipe_hiperta_r0_to_dl1lstchain {cc}',
+                   f'-i {file}',
+                   f'-o {args.output_dir}',
+                   f'-k {args.keep_file}',
+                   f'-d {args.debug_mode}'
+                   ]
+
+            subprocess.run(cmd)
+
+
+if __name__ == '__main__':
+    main()
