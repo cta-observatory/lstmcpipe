162501dbed537c1e1d51911d883b24143e86a848
Lukas Nickel <lukas.nickel@tu-dortmund.de>
2021-09-07 17:29:44 +0000

    Fix scripts for use with the new jobarray flow

--- lstmcpipe/scripts/script_batch_filelist_lst.py
+++ lstmcpipe/scripts/script_batch_filelist_lst.py
@@ -11,7 +11,8 @@ def main():
     parser.add_argument('--file_list', '-f', type=str,
                         dest='file_list',
                         help='Path to file containing list of DL0 lstchain file to be processed.',
-                        required=True)
+                        required=True,
+                        nargs="+",)
     parser.add_argument('--output_dir', '-o', type=str,
                         dest='output_dir',
                         help='lstchain_mc_r0_to_dl1 output directory argument.',
@@ -27,9 +28,8 @@ def main():
     print("Using file: ", file_for_this_job)
 
     # lstchain takes the output dir and constructs filenanmes itself
-    with open(args.file_list, 'r') as filelist:
+    with open(file_for_this_job, 'r') as filelist:
         for file in filelist:
-
             file = file.strip('\n')
 
             cmd = ['lstchain_mc_r0_to_dl1',
