0e7ca7fc4ff5f9bd914774154cc26cd686207ee5
Thomas Vuillaume <thomas.vuillaume@lapp.in2p3.fr>
2022-02-23 18:41:23 +0100

    rm skipif

--- lstmcpipe/tests/test_configs.py
+++ lstmcpipe/tests/test_configs.py
@@ -1,6 +1,7 @@
 import pytest
 from pathlib import Path
 from  lstmcpipe.config import load_config
+from lstchain.io.config import read_configuration_file
 
 PROD_CONFIGS_DIR = Path(__file__).parent.joinpath('../../production_configs/')
 
@@ -10,8 +11,6 @@ def test_all_lstmcpipe_configs():
         load_config(lstmcpipe_conf_file)
 
 
-@pytest.importorskip("lstchain", reason="lstchain not installed")
 def test_all_lstchain_configs():
-    from lstchain.io.config import read_configuration_file
     for lstchain_conf_file in PROD_CONFIGS_DIR.glob('*/*.json'):
         read_configuration_file(lstchain_conf_file)
