62f048823cbc5d5c2e9d6e9fae193834ef4115cd
vuillaut <thomas.vuillaume@gmail.com>
2022-04-08 11:55:40 +0200

    script to generate a mini prod

---
+++ lstmcpipe/scripts/generate_test_lapalma.py
@@ -0,0 +1,34 @@
+"""
+Script to generate a mini production tree with symlinks to a few simtel files and the corresponding path config.
+This is used to test lstmcpipe on a small prod
+"""
+
+from pathlib import Path
+from tqdm import tqdm
+from datetime import date
+
+from lstmcpipe.config import paths_config
+
+lstmcpipe_base_dir = Path('/fefs/aswg/workspace/lstmcpipe/data/test_data/mc/')
+aswg_base_dir = Path('/fefs/aswg/data/mc/')
+
+nfiles = 4
+
+for particle in tqdm(['gamma-diffuse', 'proton', 'electron', 'gamma']):
+    for i, file in enumerate(
+            aswg_base_dir.glob(f'DL0/20200629_prod5_trans_80/{particle}/zenith_20deg/south_pointing/**/*.simtel.gz')):
+        if i >= nfiles:
+            break
+        else:
+            target = lstmcpipe_base_dir.joinpath(file.relative_to(aswg_base_dir))
+            target.parent.mkdir(parents=True, exist_ok=True)
+            if not target.exists():
+                target.symlink_to(file)
+
+pc = paths_config.PathConfigProd5Trans80(f'test_prod_{date.today()}')
+
+pc.base_dir = '/fefs/aswg/workspace/lstmcpipe/data/test_data/mc/{data_level}/20200629_prod5_trans_80/{particle}/{zenith}/south_pointing/{prod_id}'
+
+pc.generate()
+
+pc.save_yml(f'test_prod_{date.today()}.yaml', overwrite=True)
