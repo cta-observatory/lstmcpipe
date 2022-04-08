56d58363ff8b5c5d4dbfb7736b60461f837ab56c
Thomas Vuillaume <thomas.vuillaume@lapp.in2p3.fr>
2022-04-08 17:46:01 +0200

    fix parsing to find different gammas

--- lstmcpipe/scripts/generate_test_lapalma.py
+++ lstmcpipe/scripts/generate_test_lapalma.py
@@ -6,6 +6,7 @@ This is used to test lstmcpipe on a small prod
 from pathlib import Path
 from tqdm import tqdm
 from datetime import date
+import os
 
 from lstmcpipe.config import paths_config
 
@@ -15,15 +16,15 @@ aswg_base_dir = Path('/fefs/aswg/data/mc/')
 nfiles = 4
 
 for particle in tqdm(['gamma-diffuse', 'proton', 'electron', 'gamma']):
-    for i, file in enumerate(
-            aswg_base_dir.glob(f'DL0/20200629_prod5_trans_80/{particle}/zenith_20deg/south_pointing/**/*.simtel.gz')):
-        if i >= nfiles:
-            break
-        else:
-            target = lstmcpipe_base_dir.joinpath(file.relative_to(aswg_base_dir))
-            target.parent.mkdir(parents=True, exist_ok=True)
-            if not target.exists():
-                target.symlink_to(file)
+    for root, dirs, files in os.walk(aswg_base_dir.joinpath(f'DL0/20200629_prod5_trans_80/{particle}/zenith_20deg/south_pointing')):
+        simtel_files = [os.path.join(root, file) for file in files if file.endswith('.simtel.gz')]
+        if simtel_files:
+            for file in simtel_files[:nfiles]:
+                target = lstmcpipe_base_dir.joinpath(Path(file).relative_to(aswg_base_dir))
+                target.parent.mkdir(parents=True, exist_ok=True)
+                if not target.exists():
+                    target.symlink_to(file)
+                                     
 
 pc = paths_config.PathConfigProd5Trans80(f'test_prod_{date.today()}')
 
@@ -32,3 +33,4 @@ pc.base_dir = '/fefs/aswg/workspace/lstmcpipe/data/test_data/mc/{data_level}/202
 pc.generate()
 
 pc.save_yml(f'test_prod_{date.today()}.yaml', overwrite=True)
+
