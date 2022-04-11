9e0f753613bbfbc9315900c8e5d5a84f72a60171
vuillaut <thomas.vuillaume@gmail.com>
2022-04-11 11:41:22 +0200

    write prod_type from class name in the config file

--- lstmcpipe/config/tests/test_paths_config.py
+++ lstmcpipe/config/tests/test_paths_config.py
@@ -53,7 +53,9 @@ def test_path_config_save():
     pcfg.paths = {'a': 'rick'}
     with tempfile.NamedTemporaryFile() as f:
         pcfg.save_yml(f.name, overwrite=True)
-        YAML().load(open(f.name).read())
+        loaded_cfg = YAML().load(open(f.name).read())
+    assert loaded_cfg['prod_type'] == 'PathConfig'
+    assert loaded_cfg['prod_id'] == 'v00'
 
 
 def test_PathConfigProd5Trans80():
