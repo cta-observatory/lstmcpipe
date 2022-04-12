c37bfc85060725d8ddf5fa9c2c05b5945a7394a8
vuillaut <thomas.vuillaume@gmail.com>
2022-04-12 22:19:18 +0200

    use environment variable LSTMCPIPE_PROD_LOGS

--- lstmcpipe/__init__.py
+++ lstmcpipe/__init__.py
@@ -1,4 +1,8 @@
+import os
 from pathlib import Path
 
 lstmcpipe_root_dir = Path(__file__).absolute().parents[1]
+prod_logs = os.getenv('LSTMCPIPE_PROD_LOGS') if os.getenv('LSTMCPIPE_PROD_LOGS') is not None \
+    else os.path.join(os.getenv('HOME'), 'LSTMCPIPE_PROD_LOGS')
+
 __version__ = "0.6.1"
