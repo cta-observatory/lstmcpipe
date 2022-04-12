3efb23bbdfdc86bbbde08bdc091458aacdf35dae
Thomas Vuillaume <thomas.vuillaume@lapp.in2p3.fr>
2022-04-12 15:40:53 +0200

    complete refactor of the script to make it more general

--- lstmcpipe/scripts/generate_test_lapalma.py
+++ lstmcpipe/scripts/generate_test_lapalma.py
@@ -7,30 +7,74 @@ from pathlib import Path
 from tqdm import tqdm
 from datetime import date
 import os
+import argparse
 
 from lstmcpipe.config import paths_config
 
-lstmcpipe_base_dir = Path('/fefs/aswg/workspace/lstmcpipe/data/test_data/mc/')
-aswg_base_dir = Path('/fefs/aswg/data/mc/')
 
-nfiles = 4
 
-for particle in tqdm(['gamma-diffuse', 'proton', 'electron', 'gamma']):
-    for root, dirs, files in os.walk(aswg_base_dir.joinpath(f'DL0/20200629_prod5_trans_80/{particle}/zenith_20deg/south_pointing')):
+def generate_tree(base_dir, working_dir, nfiles):
+    """
+    Walk the base dir looking for simtels files
+    When a directory contains simtels files, it's tree structure is duplicated into the working dir and nfiles are symlinked there
+    """
+    base_dir = Path(base_dir)
+    working_dir = Path(working_dir)
+    
+    for root, dirs, files in tqdm(os.walk(base_dir)):
         simtel_files = [os.path.join(root, file) for file in files if file.endswith('.simtel.gz')]
         if simtel_files:
             for file in simtel_files[:nfiles]:
-                target = lstmcpipe_base_dir.joinpath(Path(file).relative_to(aswg_base_dir))
+                target = working_dir.joinpath(Path(file).relative_to(base_dir))
                 target.parent.mkdir(parents=True, exist_ok=True)
                 if not target.exists():
                     target.symlink_to(file)
-                                     
 
-pc = paths_config.PathConfigProd5Trans80(f'test_prod_{date.today()}')
 
-pc.base_dir = '/fefs/aswg/workspace/lstmcpipe/data/test_data/mc/{data_level}/20200629_prod5_trans_80/{particle}/{zenith}/south_pointing/{prod_id}'
 
-pc.generate()
+def generate_test_prod5trans80(working_dir = '/fefs/aswg/workspace/lstmcpipe/data/mc/', nfiles=5, path_to_config_file='.', overwrite=True):
+    base_dir = '/fefs/aswg/workspace/lstmcpipe/data/test_data/mc/DL0/20200629_prod5_trans_80/'
+    working_dir = os.path.join(working_dir, 'DL0/20200629_prod5_trans_80/')
+    
+    generate_tree(base_dir, working_dir, nfiles)
 
-pc.save_yml(f'test_prod_{date.today()}.yaml', overwrite=True)
+    pc = paths_config.PathConfigProd5Trans80(f'test_prod_{date.today()}')
+    pc.base_dir = os.path.join(working_dir, '{data_level}/20200629_prod5_trans_80/{particle}/{zenith}/south_pointing/{prod_id}')
+    pc.generate()
+    pc.save_yml(os.path.join(path_to_config_file, f'test_prod5trans80_{date.today()}.yaml'), overwrite=overwrite)
 
+
+def generate_test_allsky(working_dir = '/fefs/aswg/workspace/lstmcpipe/data/mc/', nfiles=5, path_to_config_file='.', overwrite=True):
+    base_dir = '/home/georgios.voutsinas/ws/AllSky/'
+    working_dir = os.path.join(working_dir, 'AllSky')
+    
+    generate_tree(base_dir, working_dir, nfiles)
+
+    pc = paths_config.PathConfigAllSky(f'test_prod_{date.today()}')
+    pc.base_dir = os.path.join(working_dir, '{data_level}/{prod_id}/{particle}/{pointing}')
+    pc.generate()
+    pc.save_yml(os.path.join(path_to_config_file, f'test_AllSky_{date.today()}.yaml'), overwrite=overwrite)
+
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Generate test tree')
+
+    parser.add_argument('prod_type', type=str, 
+                        help='prod5trans80 or allsky')
+    parser.add_argument('--nfiles', type=int, default=5,
+                        help='Number of files')
+    parser.add_argument('--path_config_file', type=Path, default='.',
+                        help='Path to save the corresponding config file')
+    parser.add_argument('--working_dir', type=Path, default='/fefs/aswg/workspace/lstmcpipe/data/mc/',
+                        help='Your working dir')
+
+    args = parser.parse_args()
+    
+    if args.prod_type == 'prod5trans80':
+        generate_test_prod5trans80(args.working_dir, args.nfiles, args.path_config_file)
+    elif args.prod_type == 'allsky':
+        generate_test_allsky(args.working_dir, args.nfiles, args.path_config_file)
+    else:
+        raise NotImplementedError("Unknown prod type")
