6ed05f4dd3c122bf8e3d31fa8258edda3c75a012
vuillaut <thomas.vuillaume@gmail.com>
2022-04-14 15:33:59 +0200

    sshhh  pylint

--- lstmcpipe/__init__.py
+++ lstmcpipe/__init__.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from .version import __version__
+from .version import __version__  # pylint: disable=unused-import
 
 lstmcpipe_root_dir = Path(__file__).absolute().parents[1]
 
