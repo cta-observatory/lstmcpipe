98129fb43641f82ae24ca5f02698f95ecbc42648
vuillaut <thomas.vuillaume@gmail.com>
2022-04-14 15:50:48 +0200

    will i get crazy?

--- lstmcpipe/__init__.py
+++ lstmcpipe/__init__.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from .version import __version__  # noqa
+from .version import __version__  # noqa # pylint: disable=unused-import
 
 lstmcpipe_root_dir = Path(__file__).absolute().parents[1]
 
