e97f3f0ce80f6a55085028d1f699d999b72b7915
vuillaut <thomas.vuillaume@gmail.com>
2022-05-12 09:38:55 +0200

    simplpify

--- lstmcpipe/tests/test_utils.py
+++ lstmcpipe/tests/test_utils.py
@@ -1,22 +1,17 @@
 from lstmcpipe.utils import rerun_cmd
 import tempfile
 from pathlib import Path
-import subprocess
-import sys
 
 def test_rerun_cmd():
-    def faulty_func(outfile):
-        sys.exit(-1)
-
 
     with tempfile.TemporaryDirectory() as tmp_dir:
-        file, filename = tempfile.mkstemp()
+        file, filename = tempfile.mkstemp(dir=tmp_dir)
         cmd = f'echo "1" >> {filename}; rm nonexistingfile'
         # first test: the cmd fails 3 times but the outfile stays in place
         subdir_failures = ''
         rerun_cmd(cmd, filename, max_ntry=3, subdir_failures=subdir_failures)
         filename = Path(filename)
-        filename = filename.parent.joinpath(subdir_failures).joinpath(filename.name)
+        filename = Path(tmp_dir).joinpath(subdir_failures, filename.name)
         assert open(filename).read() == "1\n1\n1\n"
         # 2nd test: the cmd fails and the outfile is move in subdir
         subdir_failures = 'fail'
