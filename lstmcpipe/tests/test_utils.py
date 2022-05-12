7c42a906cd21746b5c9ec1fe3839568a767ce20e
vuillaut <thomas.vuillaume@gmail.com>
2022-05-12 10:36:29 +0200

    all run kwargs

--- lstmcpipe/tests/test_utils.py
+++ lstmcpipe/tests/test_utils.py
@@ -9,13 +9,13 @@ def test_rerun_cmd():
         cmd = f'echo "1" >> {filename}; rm nonexistingfile'
         # first test: the cmd fails 3 times but the outfile stays in place
         subdir_failures = ''
-        rerun_cmd(cmd, filename, max_ntry=3, subdir_failures=subdir_failures)
+        rerun_cmd(cmd, filename, max_ntry=3, subdir_failures=subdir_failures, shell=True)
         filename = Path(filename)
         filename = Path(tmp_dir).joinpath(subdir_failures, filename.name)
         assert open(filename).read() == "1\n1\n1\n"
         # 2nd test: the cmd fails and the outfile is move in subdir
         subdir_failures = 'fail'
-        rerun_cmd(cmd, filename, max_ntry=3, subdir_failures=subdir_failures)
+        rerun_cmd(cmd, filename, max_ntry=3, subdir_failures=subdir_failures, shell=True)
         filename = filename.parent.joinpath(subdir_failures).joinpath(filename.name)
         assert open(filename).read() == "1\n"
         assert filename.exists()
--- lstmcpipe/utils.py
+++ lstmcpipe/utils.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import subprocess
 
 
-def rerun_cmd(cmd, outfile, max_ntry=2, subdir_failures='failed_outputs'):
+def rerun_cmd(cmd, outfile, max_ntry=2, subdir_failures='failed_outputs', **run_kwargs):
     """
     rerun r0_to_dl1 process given by `cmd` as long as the exit code is 0 and number of try < max_ntry
     move the failed output file to subdir failed_outputs
@@ -11,15 +11,17 @@ def rerun_cmd(cmd, outfile, max_ntry=2, subdir_failures='failed_outputs'):
     ----------
     cmd: str
     simtel_file: Path
+    subdir_failures: str
     outfile: Path
         path to the cmd output file
     max_ntry: int
+    run_kwargs: kwargs for subprocess.run
     """
     outfile = Path(outfile)
     ret = -1
     ntry = 1
     while ret != 0 and ntry <= max_ntry:
-        ret = subprocess.run(cmd, shell=True)
+        ret = subprocess.run(cmd, **run_kwargs)
         if ret.returncode != 0:
             failed_jobs_subdir = Path(outfile.parent).joinpath(subdir_failures)
             if outfile.exists():
