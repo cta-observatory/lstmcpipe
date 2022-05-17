3567c8037d5c992077c0b260d0c72fd1808c34e7
Enrique Garcia <garcia@lapp.in2p3.fr>
2022-05-17 18:14:14 +0200

    add spaces in final slurm constructor

--- lstmcpipe/slurm_utils.py
+++ lstmcpipe/slurm_utils.py
@@ -49,12 +49,12 @@ class SbatchLstMCStage:
     ):
         self.base_slurm_command = "sbatch --parsable"
 
-        self.slurm_output = f" --output={slurm_output}" if slurm_output is not None else " --output=./slurm-%j.o"
-        self.slurm_error = f" --error={slurm_error}" if slurm_error is not None else " --error=./slurm-%j.e"
-        self.job_name = f" --job-name={job_name}" if job_name is not None else ""
-        self.slurm_account = f" --account={slurm_account}" if slurm_account is not None else ""
+        self.slurm_output = f"--output={slurm_output}" if slurm_output is not None else "--output=./slurm-%j.o"
+        self.slurm_error = f"--error={slurm_error}" if slurm_error is not None else "--error=./slurm-%j.e"
+        self.job_name = f"--job-name={job_name}" if job_name is not None else ""
+        self.slurm_account = f"--account={slurm_account}" if slurm_account is not None else ""
 
-        self.slurm_partition = f" --partition={slurm_account}" if slurm_partition is not None else " --partition=short"
+        self.slurm_partition = f"--partition={slurm_account}" if slurm_partition is not None else "--partition=short"
         self.slurm_options = f"{slurm_options}" if slurm_options is not None else None
 
         self.wrap_cmd = None
@@ -86,23 +86,23 @@ class SbatchLstMCStage:
         if wrap_command is None:
             raise ValueError("You must pass a command to be batched! ")
         if source_env != "" and not source_env.strip().endswith(";"):
-            source_env += "; "
+            source_env = source_env.strip() + "; "
         if backend != "" and not backend.strip().endswith(";"):
-            backend += "; "
-        self.wrap_cmd = f' --wrap="{backend}{source_env}{wrap_command}"'
+            backend = backend.strip() + "; "
+        self.wrap_cmd = f'--wrap="{backend}{source_env}{wrap_command}"'
 
     def _slurm_command(self):
         if self.slurm_options is not None:
             return (
-                f"{self.base_slurm_command}{self.job_name}{self.slurm_options}"
-                f"{self.slurm_error}{self.slurm_output}{self.slurm_dependencies}"
-                f"{self.slurm_account}{self.wrap_cmd}"
+                f"{self.base_slurm_command} {self.job_name} {self.slurm_options}"
+                f" {self.slurm_error} {self.slurm_output} {self.slurm_dependencies}"
+                f" {self.slurm_account} {self.wrap_cmd}"
             )
         else:
             return (
-                f"{self.base_slurm_command}{self.job_name}{self.slurm_partition}"
-                f"{self.slurm_error}{self.slurm_output}{self.slurm_dependencies}"
-                f"{self.slurm_account}{self.wrap_cmd}"
+                f"{self.base_slurm_command} {self.job_name} {self.slurm_partition}"
+                f" {self.slurm_error} {self.slurm_output} {self.slurm_dependencies}"
+                f" {self.slurm_account} {self.wrap_cmd}"
             )
 
     def check_slurm_dependencies(self, slurm_deps, dependency="afterok"):
@@ -110,7 +110,7 @@ class SbatchLstMCStage:
             self.slurm_dependencies = ""
         else:
             if all(items != "" for items in slurm_deps.split(",")):
-                self.slurm_dependencies = f" --dependency={dependency}:{slurm_deps}"
+                self.slurm_dependencies = f"--dependency={dependency}:{slurm_deps}"
             else:
                 raise ValueError("Slurm dependencies contain an empty value between commas, i.e.; ,'', ")
 
@@ -142,45 +142,45 @@ class SbatchLstMCStage:
             return jobid
 
     def r0_dl1_options(self, process_dl1_job_name="r0_dl1", array="0-0%100"):
-        self.job_name = f" --job-name={process_dl1_job_name}"
-        self.slurm_options = f" --partition=long --array={array}"
+        self.job_name = f"--job-name={process_dl1_job_name}"
+        self.slurm_options = f"--partition=long --array={array}"
 
     def dl1ab_options(self, process_dl1ab_job_name="dl1ab", array="0-0%100"):
-        self.job_name = f" --job-name={process_dl1ab_job_name}"
-        self.slurm_options = f" --partition=long --array={array}"
+        self.job_name = f"--job-name={process_dl1ab_job_name}"
+        self.slurm_options = f"--partition=long --array={array}"
 
     def merge_dl1_default_options(self):
-        self.job_name = " --job-name=merge"
-        self.slurm_partition = " --partition=long"
+        self.job_name = "--job-name=merge"
+        self.slurm_partition = "--partition=long"
 
     def train_test_splitting_default_options(self):
-        self.job_name = " --job-name=train_test_splitting"
-        self.slurm_partition = " --partition=short"
+        self.job_name = "--job-name=train_test_splitting"
+        self.slurm_partition = "--partition=short"
 
     def trainpipe_default_options(self):
-        self.job_name = " --job-name=train_pipe"
+        self.job_name = "--job-name=train_pipe"
         # self.slurm_options = " --partition=long --mem=32G"
-        self.slurm_options = " --partition=xxl --mem=100G --cpus-per-task=16"
+        self.slurm_options = "--partition=xxl --mem=100G --cpus-per-task=16"
 
     def train_plot_rf_feat_default_options(self):
-        self.job_name = " --job-name=RF_importance"
-        self.slurm_options = " --partition=short --mem=16G"
+        self.job_name = "--job-name=RF_importance"
+        self.slurm_options = "--partition=short --mem=16G"
 
     def dl1_dl2_default_options(self):
-        self.job_name = " --job-name=dl1_2"
-        self.slurm_options = " --partition=short --mem=32G"
+        self.job_name = "--job-name=dl1_2"
+        self.slurm_options = "--partition=short --mem=32G"
 
     def dl2_irfs_default_options(self):
-        self.job_name = " --job-name=dl2_IRFs"
-        self.slurm_partition = " --partition=short"
+        self.job_name = "--job-name=dl2_IRFs"
+        self.slurm_partition = "--partition=short"
 
     def dl2_sens_default_options(self):
-        self.job_name = " --job-name=dl2_sens"
-        self.slurm_options = " --partition=short --mem=32G"
+        self.job_name = "--job-name=dl2_sens"
+        self.slurm_options = "--partition=short --mem=32G"
 
     def dl2_sens_plot_default_options(self):
-        self.job_name = " --job-name=dl2_sens_plot"
-        self.slurm_partition = " --partition=short"
+        self.job_name = "--job-name=dl2_sens_plot"
+        self.slurm_partition = "--partition=short"
 
     # def load_from_log(self, jobid):
     #     jobid = ''
