42512d6b15ed91f525470b842bca30edee297121
Enrique Garcia <garcia@lapp.in2p3.fr>
2022-05-17 15:24:59 +0200

    finish class SbatchLstMCStage

--- lstmcpipe/slurm_utils.py
+++ lstmcpipe/slurm_utils.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-import warnings
 import subprocess as sp
 
 
@@ -36,6 +35,7 @@ class SbatchLstMCStage:
 
     def __init__(
         self,
+        stage,
         slurm_output=None,
         slurm_error=None,
         job_name=None,
@@ -43,26 +43,48 @@ class SbatchLstMCStage:
         slurm_partition=None,
         slurm_deps=None,
         slurm_options=None,
+        wrap_command=None,
+        source_environment="",
+        backend="",
     ):
         self.base_slurm_command = "sbatch --parsable"
-        self.wrap_cmd = ""
 
         self.slurm_output = f" --output={slurm_output}" if slurm_output is not None else " --output=./slurm-%j.o"
         self.slurm_error = f" --error={slurm_error}" if slurm_error is not None else " --error=./slurm-%j.e"
         self.job_name = f" --job-name={job_name}" if job_name is not None else ""
         self.slurm_account = f" --account={slurm_account}" if slurm_account is not None else ""
-        self.slurm_dependencies = self.check_slurm_dependencies(slurm_deps)
 
         self.slurm_partition = f" --partition={slurm_account}" if slurm_partition is not None else " --partition=short"
         self.slurm_options = f"{slurm_options}" if slurm_options is not None else None
+
+        self.wrap_cmd = None
+        self.slurm_dependencies = None
+        self.check_slurm_dependencies(slurm_deps)
+        self.compose_wrap_command(wrap_command, source_environment, backend)
+        self.stage_default_options(stage)
 
     def __repr__(self):
         # return full string
         return self._slurm_command()
 
+    @property
+    def _valid_stages(self):
+        return [
+            "r0_dl1",
+            "dl1ab",
+            "merge_dl1",
+            "train_test_splitting",
+            "train_pipe",
+            "RF_importance",
+            "dl1_dl2",
+            "dl2_IRFs",
+            "dl2_sens",
+            "dl2_sens_plot",
+        ]
+
     def compose_wrap_command(self, wrap_command=None, source_env="", backend=""):
         if wrap_command is None:
-            raise ValueError("You must pass a command.")
+            raise ValueError("You must pass a command to be batched! ")
         if source_env != "" and not source_env.strip().endswith(";"):
             source_env += "; "
         if backend != "" and not backend.strip().endswith(";"):
@@ -70,40 +92,60 @@ class SbatchLstMCStage:
         self.wrap_cmd = f' --wrap="{backend}{source_env}{wrap_command}"'
 
     def _slurm_command(self):
-        if self.wrap_cmd == "" or self.wrap_cmd is None:
-            _wrap_cmd = " <MISSING COMMAND TO BE RUN>"
-            warnings.warn("Missing command to be batched. Add it by SbatchLstMCStage().compose_wrap_command('COMMAND')")
-        else:
-            _wrap_cmd = self.wrap_cmd
-
         if self.slurm_options is not None:
             return (
                 f"{self.base_slurm_command}{self.job_name}{self.slurm_options}"
                 f"{self.slurm_error}{self.slurm_output}{self.slurm_dependencies}"
-                f"{self.slurm_account}{_wrap_cmd}"
+                f"{self.slurm_account}{self.wrap_cmd}"
             )
         else:
             return (
                 f"{self.base_slurm_command}{self.job_name}{self.slurm_partition}"
                 f"{self.slurm_error}{self.slurm_output}{self.slurm_dependencies}"
-                f"{self.slurm_account}{_wrap_cmd}"
+                f"{self.slurm_account}{self.wrap_cmd}"
             )
 
-    @staticmethod
-    def check_slurm_dependencies(slurm_deps, dependency="afterok"):
+    def check_slurm_dependencies(self, slurm_deps, dependency="afterok"):
         if slurm_deps is None:
-            return ""
+            self.slurm_dependencies = ""
         else:
             if all(items != "" for items in slurm_deps.split(",")):
-                return f" --dependency={dependency}:{slurm_deps}"
+                self.slurm_dependencies = f" --dependency={dependency}:{slurm_deps}"
             else:
                 raise ValueError("Slurm dependencies contain an empty value between commas, i.e.; ,'', ")
 
-    def r0_dl1_default_options(self, process_dl1_job_name="r0_dl1", array="0-99%100"):
+    def stage_default_options(self, stage):
+        if stage not in self._valid_stages or stage is None:
+            raise ValueError(f"Please select a valid stage: \n{', '.join(self._valid_stages)}")
+
+        _default_options = {
+            "r0_dl1": getattr(self, "r0_dl1_options"),
+            "dl1ab": getattr(self, "dl1ab_options"),
+            "merge_dl1": getattr(self, "merge_dl1_default_options"),
+            "train_test_splitting": getattr(self, "train_test_splitting_default_options"),
+            "RF_importance": getattr(self, "train_plot_rf_feat_default_options"),
+            "dl1_dl2": getattr(self, "dl1_dl2_default_options"),
+            "dl2_IRFs": getattr(self, "dl2_irfs_default_options"),
+            "dl2_sens": getattr(self, "dl2_sens_default_options"),
+            "dl2_sens_plot": getattr(self, "dl2_sens_plot_default_options"),
+        }
+        _default_options[stage]()
+
+    @property
+    def submit(self):
+        if self.wrap_cmd is None or self.wrap_cmd == "":
+            raise ValueError(
+                "You must first define the command to be batched: " "SbatchLstMCStage().wrap_command('COMMAND')"
+            )
+        else:
+            jobid = run_command(self._slurm_command())
+            return jobid
+
+    def r0_dl1_options(self, process_dl1_job_name="r0_dl1", array="0-0%100"):
         self.job_name = f" --job-name={process_dl1_job_name}"
         self.slurm_options = f" --partition=long --array={array}"
 
-    def dl1ab_default_options(self, process_dl1ab_job_name="r0_dl1", array="0-99%100"):
+    def dl1ab_options(self, process_dl1ab_job_name="dl1ab", array="0-0%100"):
         self.job_name = f" --job-name={process_dl1ab_job_name}"
         self.slurm_options = f" --partition=long --array={array}"
 
@@ -112,7 +154,7 @@ class SbatchLstMCStage:
         self.slurm_partition = " --partition=long"
 
     def train_test_splitting_default_options(self):
-        self.job_name = " --job-name=splitting_train_test"
+        self.job_name = " --job-name=train_test_splitting"
         self.slurm_partition = " --partition=short"
 
     def trainpipe_default_options(self):
@@ -120,7 +162,7 @@ class SbatchLstMCStage:
         # self.slurm_options = " --partition=long --mem=32G"
         self.slurm_options = " --partition=xxl --mem=100G --cpus-per-task=16"
 
-    def train_plot_rf_feat_default_optios(self):
+    def train_plot_rf_feat_default_options(self):
         self.job_name = " --job-name=RF_importance"
         self.slurm_options = " --partition=short --mem=16G"
 
@@ -139,16 +181,6 @@ class SbatchLstMCStage:
     def dl2_sens_plot_default_options(self):
         self.job_name = " --job-name=dl2_sens_plot"
         self.slurm_partition = " --partition=short"
-
-    @property
-    def submit(self):
-        if self.wrap_cmd is None or self.wrap_cmd == "":
-            raise ValueError(
-                "You must first define the command to be batched: " "SbatchLstMCStage().wrap_command('COMMAND')"
-            )
-        else:
-            jobid = run_command(self._slurm_command())
-            return jobid
 
     # def load_from_log(self, jobid):
     #     jobid = ''
