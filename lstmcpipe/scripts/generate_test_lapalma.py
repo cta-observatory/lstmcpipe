f803abecfe40b43172fbf30a01264a628de48ba9
vuillaut <thomas.vuillaume@gmail.com>
2022-12-12 20:32:10 +0100

    gix lapalma test script for new prod path

--- lstmcpipe/scripts/generate_test_lapalma.py
+++ lstmcpipe/scripts/generate_test_lapalma.py
@@ -32,9 +32,7 @@ def generate_tree(base_dir, working_dir, nfiles):
                     target.symlink_to(file)
 
 
-def generate_test_prod5trans80(
-    working_dir, nfiles=5, path_to_config_file='.', overwrite=True
-):
+def generate_test_prod5trans80(working_dir, nfiles=5, path_to_config_file='.', overwrite=True):
     base_dir = '/fefs/aswg/workspace/lstmcpipe/data/test_data/mc/DL0/20200629_prod5_trans_80/'
     working_dir = os.path.join(working_dir, 'DL0/20200629_prod5_trans_80/')
 
@@ -55,14 +53,16 @@ def generate_test_allsky(
     decs=['dec_4822', 'dec_931'],
     overwrite=True,
 ):
-    allsky_train_base_dir = '/home/georgios.voutsinas/ws/AllSky'
-    allsky_test_base_dir = '/home/georgios.voutsinas/ws/AllSky'
+    allsky_train_base_dir = '/fefs/aswg/data/mc/DL0/LSTProd2/'
+    allsky_test_base_dir = '/fefs/aswg/data/mc/DL0/LSTProd2/'
 
     generate_tree(allsky_train_base_dir, os.path.join(working_dir, 'DL0/AllSky'), nfiles)
 
     pc = paths_config.PathConfigAllSkyFull(f'test_prod_{date.today()}', decs)
-    pcdl1ab = paths_config.PathConfigAllSkyFullDL1ab(f'test_prod_{date.today()}', f'test_prod_{date.today()}_dl1ab', decs, run_checker=False)
-    
+    pcdl1ab = paths_config.PathConfigAllSkyFullDL1ab(
+        f'test_prod_{date.today()}', f'test_prod_{date.today()}_dl1ab', decs, run_checker=False
+    )
+
     # config training dir are replaced with local ones
     for dec in decs:
         pc.train_configs[dec].base_dir = os.path.join(
@@ -101,19 +101,18 @@ def generate_test_allsky(
         pcdl1ab.test_configs[dec].source_config.testing_dir = os.path.join(
             working_dir, pc.test_configs[dec].testing_dir.replace(allsky_test_base_dir, 'DL0/AllSky/')
         )
-        
+
     pc.generate()
     for stage_name, stage_steps in pc.paths.items():
         for step in stage_steps:
             step['extra_slurm_options'] = {'partition': 'short'}
     pc.save_yml(os.path.join(path_to_config_file, f'test_AllSky_{date.today()}.yaml'), overwrite=overwrite)
-    
+
     pcdl1ab.generate()
     for stage_name, stage_steps in pcdl1ab.paths.items():
         for step in stage_steps:
             step['extra_slurm_options'] = {'partition': 'short'}
     pcdl1ab.save_yml(os.path.join(path_to_config_file, f'test_AllSky_{date.today()}_dl1ab.yaml'), overwrite=overwrite)
-
 
 
 if __name__ == '__main__':
@@ -150,5 +149,5 @@ if __name__ == '__main__':
         generate_test_allsky(working_dir, args.nfiles, args.path_config_file)
     else:
         raise NotImplementedError("Unknown prod type")
-        
+
     dump_lstchain_std_config(filename=Path(args.path_config_file, 'lstchain_config.json'), overwrite=True)
