520864049d8805712ddbe9861a45ed75b50f004d
vuillaut <thomas.vuillaume@gmail.com>
2024-01-26 11:29:39 +0100

    modify scripts to build cli doc

--- lstmcpipe/scripts/script_lstmcpipe_validate_config.py
+++ lstmcpipe/scripts/script_lstmcpipe_validate_config.py
@@ -19,7 +19,7 @@ def mc_r0_path():
 
 
 def run_lstchain_mc_r0_dl1(simtel_file, outdir, config_file):
-    cmd = ['lstchain_mc_r0_to_dl1', '-f', simtel_file, '-o', outdir, '-c', config_file]
+    cmd = ["lstchain_mc_r0_to_dl1", "-f", simtel_file, "-o", outdir, "-c", config_file]
     return subprocess.run(cmd)
 
 
@@ -29,8 +29,8 @@ def test_r0_dl1(config_file):
 
 
 def mc_requirements(config):
-    image_extractor = config['image_extractor']
-    if not config[image_extractor]['apply_integration_correction']:
+    image_extractor = config["image_extractor"]
+    if not config[image_extractor]["apply_integration_correction"]:
         raise ValueError(
             f"Integration correction must be applied to MC for the image extractor:\n"
             f"config['{image_extractor}']['apply_integration_correction'] = True"
@@ -39,12 +39,12 @@ def mc_requirements(config):
 
 def test_dl1ab(config_file):
     r0_path = mc_r0_path()
-    dl1_filename = 'dl1_' + Path(r0_path).name.replace('.simtel.gz', '.h5')
+    dl1_filename = "dl1_" + Path(r0_path).name.replace(".simtel.gz", ".h5")
     tmpdir = tempfile.mkdtemp()
     dl1file = Path(tmpdir, dl1_filename)
-    dl1fileout = Path(tmpdir, 'dl1ab.h5')
+    dl1fileout = Path(tmpdir, "dl1ab.h5")
     run_lstchain_mc_r0_dl1(r0_path, tmpdir, config_file)
-    cmd = ['lstchain_dl1ab', '-f', dl1file, '-o', dl1fileout, '-c', config_file]
+    cmd = ["lstchain_dl1ab", "-f", dl1file, "-o", dl1fileout, "-c", config_file]
     subprocess.run(cmd)
 
 
@@ -52,41 +52,45 @@ def validate_lstchain(config_file, stage):
     config = read_configuration_file(config_file)
     mc_requirements(config)
 
-    if stage == 'r0_to_dl1':
+    if stage == "r0_to_dl1":
         test_r0_dl1(config_file)
-    elif stage == 'dl1ab':
+    elif stage == "dl1ab":
         test_dl1ab(config_file)
 
 
-def main():
+def build_argparser():
     parser = argparse.ArgumentParser(description="Validate your lstmcpipe config")
 
     # Required arguments
     parser.add_argument(
-        dest='lstmcpipe_config_filename',
+        dest="lstmcpipe_config_filename",
         type=Path,
-        help='Path to lstmcpipe config file',
+        help="Path to lstmcpipe config file",
     )
 
     # optional
     parser.add_argument(
-        '--lstchain_config',
-        '-lc',
-        action='store',
+        "--lstchain_config",
+        "-lc",
+        action="store",
         type=Path,
-        dest='lstchain_config_filename',
-        help='Path to the lstchain config file.',
+        dest="lstchain_config_filename",
+        help="Path to the lstchain config file.",
         default=None,
     )
+    return parser
 
+
+def main():
+    parser = build_argparser()
     args = parser.parse_args()
 
     config = load_config(args.lstmcpipe_config_filename)
     if args.lstchain_config_filename is not None:
-        validate_lstchain(args.lstchain_config_filename, stage=config['stages_to_run'][0])
+        validate_lstchain(args.lstchain_config_filename, stage=config["stages_to_run"][0])
 
     print("\n\nValid configs :)")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
